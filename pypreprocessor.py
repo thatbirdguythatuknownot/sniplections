b85f2e7bb159cea9120f1590966162f5277568d4
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-01-22 09:55:36 +0800

    Update pypreprocessor.py

--- pypreprocessor.py
+++ pypreprocessor.py
@@ -1,7 +1,9 @@
 import string
 from regex import compile as pcompile, sub
 letters = {*string.ascii_letters}
-match_macro = pcompile(r'\$(\w+)(\((?:[^()]++|(?0))*+\))?\$').finditer
+macropattern = pcompile(r'\$(\w+)(\((?:[^()]++|(?0))*+\))?\$')
+match_macros = macropattern.finditer
+has_macro = macropattern.search
 
 DEFAULT_DEFINE = {'_LINE': None, '_FILE': False}
 new_defines = DEFAULT_DEFINE.copy
@@ -33,11 +35,13 @@ def py_preprocessor(s, filename='<string>'):
                         list_of_params = []
                         apn = list_of_params.append
                         assert x.find(')') != -1, f"not valid macro in line {i+1}"
+                        end_paren = x.find(')')
                         while 1:
                             y = x.find(',')
-                            if y != -1:
+                            if y != -1 and y < end_paren:
                                 param = x[:y].strip()
                                 x = x[y + 1:]
+                                end_paren -= y
                                 assert param, f"cannot have empty macro param in line {i+1}"
                                 assert param[0] in letters and (not param[1:] or param[1:].isalnum()), f"not valid macro param in line {i+1}"
                                 apn(param)
@@ -66,30 +70,31 @@ def py_preprocessor(s, filename='<string>'):
             else:
                 raise NameError(f"macro name '{x}' not defined")
         else:
-            minus_pos = 0
-            for pat in match_macro(x):
-                name, args = pat.groups()
-                if args:
-                    assert name in funcdefines, f"macro name '{name}' is not a macro function name in line {i+1}"
-                    args = list(map(str.strip, args[1:-1].split(',')))
-                    if args and args[-1] == '':
-                        args = args[:-1]
-                    assert all(args), f"cannot have empty argument in macro function call to {name} in line {i+1}"
-                    param_list, replacement = funcdefines[name]
-                    assert len(param_list) == len(args), f"length of arguments in macro function call to {name} does not match parameters length in line {i+1}"
-                    for arg, param in zip(args, param_list):
-                        replacement = sub(f"`{param}`", arg, replacement)
-                    x = x[:pat.start()] + replacement + x[pat.end():]
-                else:
-                    assert name in defines, f"macro name '{name}' is not a macro name in line {i+1}"
-                    x = (x[:(patst := pat.start()) - minus_pos]
-                        + (repl := f"{i + 1}"
-                           if defines[name] is None
-                           else (f"{filename!r}"
-                                 if defines[name] is False
-                                 else defines[name]))
-                        + x[(patnd := pat.end()) - minus_pos:])
-                    minus_pos += (patnd - patst) - len(repl)
+            while has_macro(x):
+                minus_pos = 0
+                for pat in match_macros(x):
+                    name, args = pat.groups()
+                    if args:
+                        assert name in funcdefines, f"macro name '{name}' is not a macro function name in line {i+1}"
+                        args = list(map(str.strip, args[1:-1].split(',')))
+                        if args and args[-1] == '':
+                            args = args[:-1]
+                        assert all(args), f"cannot have empty argument in macro function call to {name} in line {i+1}"
+                        param_list, replacement = funcdefines[name]
+                        assert len(param_list) == len(args), f"length of arguments in macro function call to {name} does not match parameters length in line {i+1}"
+                        for arg, param in zip(args, param_list):
+                            replacement = sub(f"`{param}`", arg, replacement)
+                        x = x[:pat.start()] + replacement + x[pat.end():]
+                    else:
+                        assert name in defines, f"macro name '{name}' is not a macro name in line {i+1}"
+                        x = (x[:(patst := pat.start()) - minus_pos]
+                            + (repl := f"{i + 1}"
+                               if defines[name] is None
+                               else (f"{filename!r}"
+                                     if defines[name] is False
+                                     else defines[name]))
+                            + x[(patnd := pat.end()) - minus_pos:])
+                        minus_pos += (patnd - patst) - len(repl)
             apn_lines(x)
     return '\n'.join(lines)
 
