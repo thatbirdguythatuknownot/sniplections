5fa43897e3bb8cf7701b2f6e0b13960ae1f5f6c7
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-08-18 06:51:46 +0800

    Update hexdump.py

--- hexdump.py
+++ hexdump.py
@@ -5,18 +5,34 @@ import unicodedata
 
 print_noline = sys.stderr.write
 offset = 16
-offset_format = "%08x"
-format = "%02x"
+offset_format = "{:08x}"
+format = "{:02x}"
 length_ordinal = 2
-valid_formats = {"x", "X", "d"}
+valid_formats = {"x", "X", "d", "D", "b", "B", "o", "O"}
 option_name_to_option = {'format': '-f', 'offset': '-o'}
+help_message = """HEXDUMP PROGRAM
+Usage:
+hexdump [options] file[.extension]/.extension
+python hexdump [options] file[.extension]/.extension
+
+Options:
+-h, -? = Print this help message
+-f = Choose format:
+    b = B = binary format for numbers (right-aligned, zero-filled for offsets)
+    o = O = octal format for numbers (left-aligned for offsets)
+    d = D = decimal format for numbers (right-aligned for offsets)
+    X = uppercase hex format for numbers (right-aligned, zero-filled for offsets)
+    x = lowercase hex format for numbers (right-aligned, zero-filled for offsets) [DEFAULT]
+-o = how many characters in a dump line/offset number [DEFAULT 16]
+
+"""
 options = sys.argv[1:]
 if not options:
     print("Cannot execute hexdump with no inputs")
     sys.exit(1)
 
 options_set = {*options}
-valid_formats = {"X": ("%02X", "%08X", 2), "x": ("%02x", "%08x", 2), "d": ("%03d", "%10d", 3), "o": ("%03o", "%-11o", 3)}
+valid_formats = {"X": ("{:02X}", "{:08X}", 2), "x": ("{:02x}", "{:08x}", 2), "d": ("{:03d}", "{:10d}", 3), "o": ("{:03o}", "{:<11o}", 3), "b": ("{:08b}", "{:0>32b}", 2)}
 need_input = []
 
 
@@ -26,13 +42,12 @@ if '-f' in options_set:
     if options[i].isalpha():
         key = options[i]
         if key in valid_formats:
+            if key in "BDO":
+                key = key.lower()
             format, offset_format, length_ordinal = valid_formats[key]
             options.remove(key)
         else:
-            if key == 'b':
-                print("Option 'b' (binary) for formatting is not yet supported")
-            else:
-                print("Option input '{key}' for option '-f' is not a valid input")
+            print("Option input '{key}' for option '-f' is not a valid input")
             sys.exit(1)
     else:
         need_input.append((i, 'format'))
@@ -51,40 +66,12 @@ has_help = 0
 if '-h' in options_set:
     options.remove('-h')
     has_help = 1
-    print_noline("""HEXDUMP PROGRAM
-Usage:
-hexdump [options] file[.extension]/.extension
-python hexdump [options] file[.extension]/.extension
-
-Options:
--h, -? = Print this help message
--f = Choose format:
-    o = octal format for numbers (left-aligned for offsets)
-    d = decimal format for numbers (right-aligned for offsets)
-    X = uppercase hex format for numbers (right-aligned, zero-filled for offsets)
-    x = lowercase hex format for numbers (right-aligned, zero-filled for offsets) [DEFAULT]
--o = how many characters in a dump line/offset number [DEFAULT 16]
-
-""")
+    print_noline(help_message)
 
 if (not has_help) and '-?' in options_set:
     options.remove('-?')
     has_help = 1
-    print_noline("""HEXDUMP PROGRAM
-Usage:
-hexdump [options] file[.extension]/.extension
-python hexdump [options] file[.extension]/.extension
-
-Options:
--h, -? = Print this help message
--f = Choose format:
-    o = octal format for numbers (left-aligned for offsets)
-    d = decimal format for numbers (right-aligned for offsets)
-    X = uppercase hex format for numbers (right-aligned, zero-filled for offsets)
-    x = lowercase hex format for numbers (right-aligned, zero-filled for offsets) [DEFAULT]
--o = how many characters in a dump line/offset number [DEFAULT 16]
-
-""")
+    print_noline(help_message)
 
 need_input.sort(key=lambda x: x[0])
 for _, x in need_input:
@@ -96,10 +83,7 @@ for _, x in need_input:
             if inp in valid_formats:
                 format, offset_format, length_ordinal = valid_formats[inp]
             else:
-                if inp == 'b':
-                    print("Option 'b' (binary) for formatting is not yet supported")
-                else:
-                    print("Option input {inp!r} for option '-f' is not a valid input")
+                print("Option input {inp!r} for option '-f' is not a valid input")
                 sys.exit(1)
         else:
             print("Invalid input for option '-f': {inp!r}")
@@ -111,7 +95,8 @@ for _, x in need_input:
             print("Invalid input for option '-o': {inp!r}")
             sys.exit(1)
     else:
-        sys.exit(-1073741819)
+        print(f"internal error: invalid option '{x}'")
+        sys.exit(1)
 
 try:
     path = f"{os.getcwd()}\\{options[0]}"
@@ -127,11 +112,11 @@ with open(path, "r") as file:
     text = file.read()
     length = len(text)
     position = 0
-    print_noline(offset_format % position)
+    print_noline(offset_format.format(position))
     while position < length:
         contents = text[:offset]
         text = text[offset:]
-        hex = ' '.join(format % ord(x) for x in contents).ljust(offset-1+offset*length_ordinal)
+        hex = ' '.join(format.format(ord(x)) for x in contents).ljust(offset-1+offset*length_ordinal)
         filtered_contents = ''
         for character in contents:
             try:
@@ -142,4 +127,4 @@ with open(path, "r") as file:
                 filtered_contents += character
         print_noline(f":  {hex}  |{filtered_contents}|\n")
         position += len(contents)
-        print_noline(offset_format % position)
+        print_noline(offset_format.format(position))
