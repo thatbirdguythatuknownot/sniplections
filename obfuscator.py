2623ae3682ea8f210d788d43f04f68d4d631a0fb
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-10-29 09:48:33 +0800

    add a "no walrus" (taken=False) mode

--- obfuscator.py
+++ obfuscator.py
@@ -2,8 +2,6 @@ import importlib, re
 from functools import cache, reduce
 from math import log2, trunc
 
-obj_subclasses_repr = "__name__.__class__.__class__.__base__.__subclasses__()"
-
 """
 def gbni(b): # get builtin name index
     for i, n in enumerate(__builtins__.__dict__):
@@ -48,18 +46,30 @@ def gpf(x): # greatest . factor
 
 class Obfuscator:
     def __init__(self, taken=None):
-        if taken is None:
-            taken = {*()}
-        self.taken = taken
-        self.object_repr_pair = {
-            int: "__name__.__len__().__class__", str: "__name__.__class__",
-            type: "__name__.__class__.__class__",
-            complex: ("({0}:=({{0}}:=__name__.__ne__(__name__).__invert__()).__truediv__({{0}}:={{0}}.__neg__()).__pow__({{0}}.__truediv__({{0}}.__invert__().__neg__())).__class__)",
-                      (), ()),
-            re: ("({0}:={1}({2}))", (__import__,), ("re",)),
-            __import__: ("({0}:=__builtins__.__getattribute__({1}))", (), ("__import__",)),
-        }
-        self._nonassigned = {complex, re, __import__}
+        self.no_walrus = taken is False
+        if self.no_walrus:
+            self.object_repr_pair = {
+                int: "__name__.__len__().__class__", str: "__name__.__class__",
+                type: "__name__.__class__.__class__",
+                object: "__name__.__class__.__class__.__base__)",
+                complex: "__name__.__ne__(__name__).__invert__().__truediv__(__name__.__eq__(__name__)).__pow__(__name__.__eq__(__name__).__truediv__(__name__.__eq__(__name__).__invert__().__neg__())).__class__",
+                re: ("{1}({2})", (__import__,), ("re",)),
+                __import__: ("__builtins__.__getattribute__({1})", (), ("__import__",)),
+            }
+        else:
+            if taken is None or taken is True:
+                taken = {*()}
+            self.taken = taken
+            self.object_repr_pair = {
+                int: "__name__.__len__().__class__", str: "__name__.__class__",
+                type: "__name__.__class__.__class__",
+                object: ("({0}:=__name__.__class__.__class__.__base__)", (), ()),
+                complex: ("({0}:=({{0}}:=__name__.__ne__(__name__).__invert__()).__truediv__({{0}}:={{0}}.__neg__()).__pow__({{0}}.__truediv__({{0}}.__invert__().__neg__())).__class__)",
+                          (), ()),
+                re: ("({0}:={1}({2}))", (__import__,), ("re",)),
+                __import__: ("({0}:=__builtins__.__getattribute__({1}))", (), ("__import__",)),
+            }
+            self._nonassigned = {complex, re, __import__}
     
     def nnu(self): # new name unmodified
         name = '_'
@@ -76,10 +86,17 @@ class Obfuscator:
         v = self.object_repr_pair[x]
         if type(v) is not tuple:
             return v
+        if self.no_walrus:
+            self.object_repr_pair[x] = res = v[0].format(
+                None,
+                *map(lambda x: self.porpv(x)[0], v[1]),
+                *map(self.gs, v[2])
+            )
+            return res, None
         if name is None:
             name = self.nn()
         self.object_repr_pair[x] = name
-        res = v[0].format(name, *map(self.porpv, v[1]), *map(self.gs, v[2]))
+        res = v[0].format(name, *map(lambda x: self.porpv(x)[0], v[1]), *map(self.gs, v[2]))
         return res, name
     
     def on(self, x, name_=None, values={
@@ -91,7 +108,11 @@ class Obfuscator:
             return f"{res}.__invert__()"
         if x in values:
             return values[x]
-        elif x not in {2, 3}:
+        if not self.no_walrus:
+            name = name_ or self.nn()
+        else:
+            name = None
+        if x not in {2, 3}:
             # TODO: better stuff
             p = gpf(x)
             add = 0
@@ -101,7 +122,6 @@ class Obfuscator:
                 p = gpf(x)
                 add += t
             q = x // p
-            name = name_ or self.nn()
             method = "mul"
             if q > 4 and (qb := log2(q)).is_integer():
                 qb = trunc(qb)
@@ -109,18 +129,26 @@ class Obfuscator:
                     pb, qb = qb, trunc(pb)
                 q = qb
                 method = "lshift"
-            res = f"({{}}:={self.on(p, name)}.__{method}__({self.on(q, name)})" \
-                  f"{f'.__add__({on(add, name)})' if add else ''})"
+            res = f"{self.on(p, name)}.__{method}__({self.on(q, name)})" \
+                  f"""{'.__invert__().__neg__()' if add == 1 else
+                     f'.__add__({on(add, name)})' if add else ''}"""
             x = orig_x # for setting to values[x] later
         else:
-            name = name_ or self.nn()
-            res = f"""({{}}:={reduce("{}.__add__({})".format,
-                                     [f"({name}:=__name__.__getitem__({on(0)}))"]
-                                     +[name]*(x-1))}.__len__())"""
+            res = f"""{reduce("{}.__add__({})".format,
+                              [f"__name__.__getitem__({on(0)})"]*x
+                              if self.no_walrus else
+                              [f"({name}:=__name__.__getitem__({on(0)}))"]
+                              +[name]*(x-1))}.__len__()"""
+        if self.no_walrus:
+            return res
         values[x] = resname = self.nn() if name_ else name
-        return res.format(resname)
+        return f"({resname}:={res})"
     
     def gdci(self, c, name_=None): # get __doc__ and character index
+        if self.no_walrus:
+            for x, rep in self.object_repr_pair.items():
+                if (r := x.__doc__.find(c)) >= 0:
+                    return f"{self.porpv(x)[0]}.__doc__", r
         name = name_ or self.nnu()
         for x, rep in self.object_repr_pair.items():
             if (r := x.__doc__.find(c)) >= 0:
@@ -136,11 +164,19 @@ class Obfuscator:
     def gs(self, s, values={}): # get string
         if s in values:
             return values[s]
+        if self.no_walrus:
+            l = []
+            for c in s:
+                rep, idx = self.gdci(c)
+                l.append(f"{rep}.__getitem__({self.on(idx)})")
+            res = reduce("{}.__add__({})".format, l)
+            values[s] = res
+            return res
         name = self.nn()
         l = []
         for c in s:
             rep, idx = self.gdci(c, name)
-            l.append(f"{rep}.__getitem__({self.on(idx)})")
+            l.append(f"{rep}.__getitem__({self.on(idx, name)})")
         res = reduce("{}.__add__({})".format, l)
         values[s] = name
         return f"({name}:={res})"
@@ -154,16 +190,24 @@ class Obfuscator:
                 (self.gs, {})]:
             (dflt := f.__defaults__[-1]).clear()
             dflt.update(new)
-        self.taken.clear()
-        self.object_repr_pair = {
-            int: "__name__.__len__().__class__", str: "__name__.__class__",
-            type: "__name__.__class__.__class__",
-            complex: ("({0}:=({{0}}:=__name__.__ne__(__name__).__invert__()).__truediv__({{0}}:={{0}}.__neg__()).__pow__({{0}}.__truediv__({{0}}.__invert__().__neg__())).__class__)",
-                      (), ()),
-            re: ("({0}:={1}({2}))", (__import__,), ("re",)),
-            __import__: ("({0}:=__builtins__.__getattribute__({1}))", (), ("__import__",)),
-        }
-        self._nonassigned = {complex, re, __import__}
-
-obfuscator = Obfuscator()
-globals().update({x: getattr(obfuscator, x) for x in dir(obfuscator) if not x.startswith('_')})
+        if self.no_walrus:
+            self.object_repr_pair = {
+                int: "__name__.__len__().__class__", str: "__name__.__class__",
+                type: "__name__.__class__.__class__",
+                object: "__name__.__class__.__class__.__base__)",
+                complex: "__name__.__ne__(__name__).__invert__().__truediv__(__name__.__eq__(__name__)).__pow__(__name__.__eq__(__name__).__truediv__(__name__.__eq__(__name__).__invert__().__neg__())).__class__",
+                re: ("{1}({2})", (__import__,), ("re",)),
+                __import__: ("__builtins__.__getattribute__({1})", (), ("__import__",)),
+            }
+        else:
+            self.taken.clear()
+            self.object_repr_pair = {
+                int: "__name__.__len__().__class__", str: "__name__.__class__",
+                type: "__name__.__class__.__class__",
+                object: ("({0}:=__name__.__class__.__class__.__base__)", (), ()),
+                complex: ("({0}:=({{0}}:=__name__.__ne__(__name__).__invert__()).__truediv__({{0}}:={{0}}.__neg__()).__pow__({{0}}.__truediv__({{0}}.__invert__().__neg__())).__class__)",
+                          (), ()),
+                re: ("({0}:={1}({2}))", (__import__,), ("re",)),
+                __import__: ("({0}:=__builtins__.__getattribute__({1}))", (), ("__import__",)),
+            }
+            self._nonassigned = {complex, re, __import__}
