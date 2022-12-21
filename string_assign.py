2ef6438687d85a006657ebe0561910b3be2d802c
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2022-12-22 07:33:04 +0800

    index checks, allow substring assignments

--- string_assign.py
+++ string_assign.py
@@ -14,30 +14,57 @@ def kind(string):
              kind = 2
      return kind
 
-def s_assign_1byte(string, otherstring):
-    assert len(string) == len(otherstring), "argument 1 must have the same length as argument 2"
+def s_assign_1byte(string, otherstring, start=0, end=None):
     assert kind(string) == 1 and kind(otherstring) <= 1, "argument 1 must have the same kind as argument 2"
-    ref = id(string)+getsizeof(string)-len(string)-1
+    len_string = len(string)
+    if start < 0:
+        start += len_string
+        assert start >= 0, "start index out of bounds"
+    if end is None: end = len_string
+    elif end < 0:
+        end += len_string
+        assert end >= 0, "end index out of bounds"
+    ref = id(string)+getsizeof(string)-len(string)+start-1
+    if (end := end - start) <= 0: return ref
     for i, x in enumerate(otherstring):
+        if i == end: break
         memset(ref+i, ord(x), 1)
     return ref
 
-def s_assign_2byte(string, otherstring):
-    assert len(string) == len(otherstring), "argument 1 must have the same length as argument 2"
+def s_assign_2byte(string, otherstring, start=0, end=None):
     assert kind(string) == 2 and kind(otherstring) <= 2, "argument 1 must have the same/greater kind as argument 2"
-    ref = id(string)+getsizeof(string)-(len(string)*2)-2
+    len_string = len(string)
+    if start < 0:
+        start += len_string
+        assert start >= 0, "start index out of bounds"
+    if end is None: end = len_string
+    elif end < 0:
+        end += len_string
+        assert end >= 0, "end index out of bounds"
+    ref = id(string)+getsizeof(string)-(len(string)+start-1)*2
+    if (end := end - start) <= 0: return ref
     for i, x in enumerate(otherstring):
+        if i == end: break
         setat = ref+2*i
         ord_x = ord(x)
         memset(setat, (o := ord_x >> 8), 2)
         memset(setat, ord_x ^ (o << 8), 1)
     return ref
 
-def s_assign_4byte(string, otherstring):
-    assert len(string) == len(otherstring), "argument 1 must have the same length as argument 2"
+def s_assign_4byte(string, otherstring, start=0, end=None):
     assert kind(string) == 4 and kind(otherstring) <= 4, "argument 1 must have the same/greater kind as argument 2"
-    ref = id(string)+getsizeof(string)-(len(string)*4)-4
+    len_string = len(string)
+    if start < 0:
+        start += len_string
+        assert start >= 0, "start index out of bounds"
+    if end is None: end = len_string
+    elif end < 0:
+        end += len_string
+        assert end >= 0, "end index out of bounds"
+    ref = id(string)+getsizeof(string)-(len(string)+start-1)*4
+    if (end := end - start) <= 0: return ref
     for i, x in enumerate(otherstring):
+        if i == end: break
         setat = ref+4*i
         ord_x = ord(x)
         memset(setat, ord_x >> 16, 3)
@@ -45,15 +72,22 @@ def s_assign_4byte(string, otherstring):
         memset(setat, ord_x & 0xff, 1)
     return ref
 
-def s_assign(string, otherstring):
-    assert len(string) == len(otherstring), "argument 1 must have the same length as argument 2"
+def s_assign(string, otherstring, start=0, end=None):
+    len_string = len(string)
+    if start < 0:
+        start += len_string
+        assert start >= 0, "start index out of bounds"
+    if end is None: end = len_string
+    elif end < 0:
+        end += len_string
+        assert end >= 0, "end index out of bounds"
     kind1 = kind(string)
     kind2 = kind(otherstring)
     if kind2 <= kind1:
         if kind1 == 1:
-            return s_assign_1byte(string, otherstring)
+            return s_assign_1byte(string, otherstring, start, end)
         elif kind1 == 2:
-            return s_assign_2byte(string, otherstring)
+            return s_assign_2byte(string, otherstring, start, end)
         else:
-            return s_assign_4byte(string, otherstring)
+            return s_assign_4byte(string, otherstring, start, end)
     raise TypeError("argument 1 kind has a lesser kind than argument 2")
