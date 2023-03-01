66759835d920edfe2b754fcb9737dda430e14be0
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2023-03-01 19:34:01 +0800

    Create min_diff_digits.py

---
+++ min_diff_digits.py
@@ -0,0 +1,31 @@
+def get_min_digits(x, digs="0123456789", n=None):
+    base = len(digs)
+    if n is None:
+        n = len(x)
+    check = {*map(digs.index, x)}
+    l_check = len(check)
+    R_l_check_d2_m1_TO_0 = range((l_check >> 1) - 1, -1, -1)
+    assert -1 not in check and l_check >= n
+    check_l = sorted(check)
+    mfdd = md = float('inf')
+    ma = mb = -1
+    for i in range(l_check - 1):
+        a0, b0 = check_l[i], check_l[i+1]
+        if (fdd := b0 - a0) > mfdd:
+            continue
+        elif fdd < mfdd:
+            mfdd = fdd
+        check_filter = check - {a0, b0}
+        a_digs, b_digs = [a0], [b0]
+        while check_filter:
+            b_, a_ = min(check_filter), max(check_filter)
+            check_filter -= {b_, a_}
+            a_digs.append(a_)
+            b_digs.append(b_)
+        a = ''.join(digs[x] for x in a_digs)
+        b = ''.join(digs[x] for x in b_digs)
+        a_intval = sum(base**x * a_digs[~x] for x in R_l_check_d2_m1_TO_0)
+        b_intval = sum(base**x * b_digs[~x] for x in R_l_check_d2_m1_TO_0)
+        if (diff := b_intval - a_intval) < md:
+            md, ma, mb = diff, a, b
+    return md, mb, ma
