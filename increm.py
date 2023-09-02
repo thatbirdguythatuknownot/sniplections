46eb6046741e38524dca0dddc5aac0caf81cc743
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2023-09-02 10:34:38 +0800

    add decrem

--- increm.py
+++ increm.py
@@ -8,35 +8,46 @@ PF_LTF.argtypes = py_object,
 PF_LTF.restype = c_int
 set_names = {*hasfree + haslocal + hasname}
 
-globals().update({x: opmap.get(x, -1) for x in ['UNARY_POSITIVE', 'CALL_INTRINSIC_1', *map(opname.__getitem__, set_names)]})
+globals().update({x: opmap.get(x, -1) for x in ['UNARY_NEGATIVE', 'UNARY_POSITIVE', 'CALL_INTRINSIC_1', *map(opname.__getitem__, set_names)]})
 
-@impl(int)
-def __pos__(self):
-    frame = _getframe(1)
+def incdec(self, inc=True):
+    frame = _getframe(2)
     a = frame.f_lasti
     code = frame.f_code.co_code
     opcode = code[a - 2]
     if opcode not in set_names:
-        return orig(int).__pos__(self)
-    if glob := opcode == LOAD_GLOBAL:
+        if (opcode is UNARY_POSITIVE or opcode is CALL_INTRINSIC_1 if inc else opcode is UNARY_NEGATIVE) and code[a - 4] in set_names:
+            return self
+        if inc:
+            return orig(int).__pos__(self)
+        return orig(int).__neg__(self)
+    if glob := opcode is LOAD_GLOBAL:
         name = frame.f_code.co_names[code[a - 1] >> 1]
-    elif opcode == LOAD_NAME:
+    elif opcode is LOAD_NAME:
         name = frame.f_code.co_names[code[a - 1]]
         glob = -1
     else:
         name = (frame.f_code.co_varnames + frame.f_code.co_cellvars + frame.f_code.co_cellvars)[code[a - 1]]
     i = 0
-    if CALL_INTRINSIC_1 < 0:
-        while code[a] is UNARY_POSITIVE:
-            i += 1
-            a += 2
+    if inc:
+        if CALL_INTRINSIC_1 < 0:
+            while code[a] is UNARY_POSITIVE:
+                i += 1
+                a += 2
+        else:
+            while code[a] is CALL_INTRINSIC_1 and code[a + 1] == 5:
+                i += 1
+                a += 2
+        if i < 2:
+            return orig(int).__pos__(self)
+        self += 1
     else:
-        while code[a] is CALL_INTRINSIC_1 and code[a + 1] == 5:
+        while code[a] is UNARY_NEGATIVE:
             i += 1
             a += 2
-    if i < 2:
-        return orig(int).__pos__(self)
-    self += 1
+        if i < 2:
+            return orig(int).__neg__(self)
+        self -= 1
     if glob is True:
         frame.f_globals[name] = self
     elif glob:
@@ -45,3 +56,11 @@ def __pos__(self):
         frame.f_locals[name] = self
         PF_LTF(frame)
     return self
+
+@impl(int)
+def __pos__(self):
+    return incdec(self)
+
+@impl(int)
+def __neg__(self):
+    return incdec(self, False)
