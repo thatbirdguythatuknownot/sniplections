4da3a7f2cf76e44f28c6c8ff518e387ef3bc7980
Crowthebird <78076854+thatbirdguythatuknownot@users.noreply.github.com>
2023-12-20 11:16:13 +0800

    Add 3.12 super() support

--- varname.py
+++ varname.py
@@ -8,7 +8,7 @@ globals().update({
         "LOAD_NAME LOAD_FAST LOAD_DEREF LOAD_CLASSDEREF LOAD_GLOBAL "
         "LOAD_ATTR CALL PRECALL EXTENDED_ARG KW_NAMES "
         "LOAD_FROM_DICT_OR_GLOBALS LOAD_FAST_CHECK LOAD_FAST_AND_CLEAR "
-        "LOAD_FROM_DICT_OR_DEREF"
+        "LOAD_FROM_DICT_OR_DEREF LOAD_SUPER_ATTR"
         .split()
 })
 if version_info == (3, 11):
@@ -46,13 +46,12 @@ def skip_header(instrs, i):
             i -= 2
     return i
 
-def varname(val):
-    frame = _out(1)
+def _varname(frame, i):
     code = frame.f_code
     instrs = code.co_code
-    i = skip_header(instrs, frame.f_lasti - ARG_OFFSET)
     qname_list = deque()
     add = qname_list.appendleft
+    addmany = qname_list.extendleft
     while i >= 0:
         opcode = instrs[i]
         oparg, i = retrieve_oparg(instrs, i)
@@ -81,6 +80,27 @@ def varname(val):
             break
         elif opcode == LOAD_ATTR:
             add('.' + code.co_names[oparg >> 1])
+        elif opcode == LOAD_SUPER_ATTR:
+            add('.' + code.co_names[oparg >> 2])
+            if oparg & 2:
+                name1, i = _varname(frame, i)
+                if name1:
+                    name0, i = _varname(frame, i)
+                    if name0:
+                        add(')')
+                        addmany(name1)
+                        add(", ")
+                        addmany(name0)
+                        add("super(")
+            else:
+                add("super()")
+            break
+    return qname_list, i
+
+def varname(val):
+    frame = _out(1)
+    i = skip_header(frame.f_code.co_code, frame.f_lasti - ARG_OFFSET)
+    qname_list, _ = _varname(frame, i)
     if not qname_list:
         raise ValueError(f"No name found for value: {val!r}")
     return "".join(qname_list)
